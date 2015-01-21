9dbd4b632dfc7eba95e40cebad3d0f0a353c50cd
David Latva <latvad@umich.edu>
2015-01-21 12:07:17 -0500

    Track looping implemented w/ teleporter
    added basic script for looping the track

---
+++ Assets/Scripts/TeleportScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleportScript : MonoBehaviour {
+	public GameObject tpExit;
+	// Use this for initialization
+	void Start () {
+		tpExit = GameObject.Find("TPExit");
+	}
+	
+	void OnTriggerEnter (Collider other){
+		Vector3 temp = other.transform.position;
+		temp.x = tpExit.transform.position.x;
+		other.transform.position = temp;
+	}
+}
