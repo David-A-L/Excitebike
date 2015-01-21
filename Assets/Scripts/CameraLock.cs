4f743c859958ec9e58c2c37d251ce0c3af77de00
Nick-Gottschlich <nickpgott@gmail.com>
2015-01-21 15:04:15 -0500

    camfix

--- Assets/Scripts/CameraLock.cs
+++ Assets/Scripts/CameraLock.cs
@@ -4,8 +4,12 @@ using System.Collections;
 public class CameraLock : MonoBehaviour {
 	//public int	dist = 10;
 	// Use this for initialization
+
+	public Transform	target;
+	public Vector3		offset;
+
 	void Start () {
-	
+		offset = this.transform.position - target.position;
 	}
 
 	//var target : Transform;
@@ -23,9 +27,16 @@ public class CameraLock : MonoBehaviour {
 		//GameObject.Find ("MainCamera").transform.position = new Vector3 (bikePos.x, bikePos.y, bikePos.z - dist);
 	}
 
+	void FixedUpdate() {
+		Vector3 temp = transform.position;
+		temp.x = target.position.x + offset.x;
+		transform.position = temp;
+	}
 	//this is glitchy, talk in office hours about how to fix this by using a script and also not having
 	//	main camera be a child of bike
 	void LateUpdate () {
+
+		/*
 		Vector3 tempPos = transform.position;
 		tempPos.z = -6.07f;
 		tempPos.y = 2.75f;
@@ -34,6 +45,7 @@ public class CameraLock : MonoBehaviour {
 		Vector3 tempRot = new Vector3 (37.68628f, 0f, 0f);
 
 		transform.rotation = Quaternion.Euler(tempRot);
+		*/
 		//transform.rotation = Quaternion.Euler(Vector3(0, 0, 0));  // 90 degress on the X axis - change appropriately*/
 	}
 }
