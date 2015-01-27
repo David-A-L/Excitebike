5a31174d830fafe120e1ff599de091cbccd925ee
David Latva <latvad@umich.edu>
2015-01-27 16:14:48 -0500

    Level Set up, added dirt, tempDown, and grass

---
+++ Assets/Scripts/Dirt.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dirt : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
