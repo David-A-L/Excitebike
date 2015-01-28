dc8b8edf638b498c9b4feaee2cb3f2cf06f974ea
Nick-Gottschlich <nickpgott@gmail.com>
2015-01-27 22:43:58 -0500

    added timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+	private float startTime;
+	string textTime;
+
+	void Awake () {
+		startTime = Time.time;
+	}
+
+	void OnGUI () {
+		float guiTime = Time.time - startTime;
+
+		int minutes = (int)guiTime / 60;
+		int seconds = (int)guiTime % 60;
+		int fracSec = (int)(guiTime * 100) % 100;
+
+		textTime = string.Format ("{0:00}:{1:00}:{2:00}", minutes, seconds, fracSec);
+		GUI.Label (new Rect (800, 550, 100, 1000), textTime); 
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
