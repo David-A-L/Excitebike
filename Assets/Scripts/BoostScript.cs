58278f538fb4d62b5b344a62dec92be006577824
David Latva <latvad@umich.edu>
2015-01-29 14:45:20 -0500

    Boost script works, needs tweaking

--- Assets/Scripts/BoostScript.cs
+++ Assets/Scripts/BoostScript.cs
@@ -3,19 +3,30 @@ using System.Collections;
 
 public class BoostScript : MonoBehaviour {
 	public GameObject boostGO;
-	public float power = 100f;
-	Vector3 dir = Vector3.zero;
+	//percentage boost applied to velocity
+	public float power = .01f;
+	//minimum speed to trigger the vertical boost
+	public float minVel = 1.5f;
+	//Vector3 dir = Vector3.zero;
 
 	void Start(){
-		dir = boostGO.transform.right;
+	//	dir = boostGO.transform.right;
 	}
 
-	void OnTriggerStay(Collider other){
-		//TODO: change direction/power based on input
+	void OnTriggerExit(Collider other){
 		if (other.gameObject.tag != "Bike")
 			return;
+
 		PE_Obj otherPEO = other.GetComponent<PE_Obj> ();
-		otherPEO.acc += dir * power;
+
+		//if moving too slow, no boost applied
+		if (otherPEO.vel.magnitude < minVel)
+			return;
+		//if holding left, boost will be applied as upward lift
+		if (Input.GetKey (KeyCode.LeftArrow) || Input.GetKey (KeyCode.A))
+			otherPEO.vel.y += otherPEO.vel.magnitude * power;
+		else 
+			otherPEO.vel += otherPEO.vel * power;
 	}
 
 }
