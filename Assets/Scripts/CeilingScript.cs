b92e5ef02f9dd6dfeb814938b2e2863e62c15a83
David Latva <latvad@umich.edu>
2015-02-03 15:12:02 -0500

    Added ceiling to custom level

---
+++ Assets/Scripts/CeilingScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CeilingScript : MonoBehaviour {
+
+	void OnTriggerStay(Collider other){
+		if (other.tag != "Bike")
+			return;
+		PE_Obj bikePEO = other.GetComponent<PE_Obj> ();
+		bikePEO.vel.y = 0;
+		bikePEO.acc.y = 0;
+	}
+}
