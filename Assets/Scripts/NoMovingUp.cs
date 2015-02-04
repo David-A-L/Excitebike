9634d65ac408faad0c28a382fda2a2d9baeeab02
David Latva <latvad@umich.edu>
2015-02-03 21:26:29 -0500

    more changes

---
+++ Assets/Scripts/NoMovingUp.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NoMovingUp : MonoBehaviour {
+	GameObject playerBike;
+	// Use this for initialization
+	void Start () {
+		playerBike = GameObject.FindGameObjectWithTag ("Bike");
+	}
+	
+	// Update is called once per frame
+
+	void OnTriggerStay(Collider other){
+
+		if (other.tag != "Bike")
+			return;
+		Bike bScript = playerBike.GetComponent<Bike> ();
+		if ( bScript.curDirIn == Bike.DirInput.UP)
+			bScript.curDirIn = Bike.DirInput.NONE;
+	}
+}
